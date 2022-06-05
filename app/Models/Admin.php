d6c0399a68a9f570f44196713df6c854126fa19d
TrungMark <trungbui0121@gmail.com>
2022-06-05 14:08:01 +0700

    update login admin mn sua ten file env de no reset pass duoc

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Admin extends Model
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+class Admin extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = ['user_login','name','email', 'phone','avatar','password', 'status'];
 }
--- resources/views/partials/silebar.blade.php
+++ resources/views/partials/silebar.blade.php
@@ -10,7 +10,8 @@
                 <img src="/adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                <a  href="{{ route('logout') }}" class="text-primary">Log Out</a>
             </div>
         </div>
         <div class="form-inline">
