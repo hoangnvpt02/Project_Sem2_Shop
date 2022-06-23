6deb9873a2aead91187fbf41207c8e86c761b958
duc-canh <84029160+duc-canh@users.noreply.github.com>
2022-06-23 14:50:42 +0700

    done23/6

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
     // protected $redirectTo = RouteServiceProvider::HOME;
     protected $redirectTo = '/admin';
 
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -57,13 +57,13 @@
 				<div class="col-md-3 clearfix">
 					<div class="header-ctn">
 						<!-- Wishlist -->
-						<div>
+						<!-- <div>
 							<a href="#">
 								<i class="fa fa-heart-o"></i>
 								<span>Your Wishlist</span>
 								<div class="qty">2</div>
 							</a>
-						</div>
+						</div> -->
 						<!-- /Wishlist -->
 
 						<!-- Cart -->
